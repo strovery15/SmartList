607885a27315fbd1bcfe76a0943621fc16c6674f
strovery15 <yutakawamae@gmail.com>
2025-08-04 02:16:30 +0900

    first commit

---
+++ SmartList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartList
+//
+//  Created by 川前優太 on 2025/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
