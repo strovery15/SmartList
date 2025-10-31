4583336371a5aa34706f07650576dd7c88d8cb5b
strovery15 <yutakawamae@gmail.com>
2025-10-31 17:47:57 +0900

    subtask-ReorderMemoTitleInteractorを新しいデータ構造に適応した形に変更した

--- SmartList/Modules/Folder/Interactor/ReorderMemoTitleInteractor.swift
+++ SmartList/Modules/Folder/Interactor/ReorderMemoTitleInteractor.swift
@@ -4,21 +4,32 @@ import Foundation
 import RealmSwift
 
 protocol ReorderMemoTitleUseCase {
-    func execute(_ parameter1: FolderEntity.ID,_ parameter2: Int,_ parameter3: Int)
+    
+    func execute(_ parameter1: MemoEntity.ID,_ parameter2: MemoEntity.ID)
 }
 
 class ReorderMemoTitleInteractor: ReorderMemoTitleUseCase {
-    func execute(_ parameter1: FolderEntity.ID,_ parameter2: Int,_ parameter3: Int) {
+    
+    func execute(_ parameter1: MemoEntity.ID,_ parameter2: MemoEntity.ID) {
         let realm = try! Realm()
-        let results = realm.objects(FolderManagerEntityRealm.self)
-        if let folderManager = results.first {
-            for (index, folderEntity) in folderManager.folderEntities.enumerated() {
-                if folderEntity.id == parameter1 {
-                    try! realm.write {
-                        folderEntity.memoTitles.move(from: parameter2, to: parameter3)
-                    }
-                }
+        let realmIdManager = realm.objects(RealmIdManagerEntity.self).first!
+        var deleteIndex = 0
+        var insertIndex = 0
+        var insertItem: RealmMemoIdEntity?
+        
+        for (index, realmMemoId) in realmIdManager.memoIds.enumerated() {
+            if realmMemoId.memoId == parameter1 {
+                deleteIndex = index
+                insertItem = realmMemoId
             }
+            if realmMemoId.memoId == parameter2 {
+                insertIndex = index
+            }
+        }
+        
+        try! realm.write {
+            realmIdManager.memoIds.remove(at: deleteIndex)
+            realmIdManager.memoIds.insert(insertItem!, at: insertIndex)
         }
     }
 }
