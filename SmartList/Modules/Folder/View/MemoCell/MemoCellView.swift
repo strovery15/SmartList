4e54b99dec841613dd6bccecf255ab83bb5a80ad
strovery15 <yutakawamae@gmail.com>
2025-11-10 23:48:52 +0900

    task-MemoCellViewを修正した
    #UIレイアウト設定をstoryboardに移した
    #Cellからの通知方法をnotificationからclosureコールバックに変更した

--- SmartList/Modules/Folder/View/MemoCell/MemoCellView.swift
+++ SmartList/Modules/Folder/View/MemoCell/MemoCellView.swift
@@ -5,25 +5,17 @@ import UIKit
 class MemoCellView: UIView, UIContentView  {
     
     @IBOutlet var view: UIView!
+    @IBOutlet weak var memoTitleLabel: UILabel!
+    @IBOutlet weak var menuButton: UIButton!
     
-    @IBOutlet weak var memoTitleLabel: UILabel! {
-        didSet {
-            configureMemoTitleLabel()
-        }
-    }
-    @IBOutlet weak var menuButton: UIButton! {
-        didSet {
-            configureMemoButton()
-        }
-    }
+    var memoId: MemoEntity.ID
     
+    var deleteBlock: ((MemoEntity.ID) -> Void)?
     
     var memoConfiguration: MemoCellConfiguration! {
         didSet {
-            if memoTitleLabel != nil && menuButton != nil {
-                configureMemoTitleLabel()
-                configureMemoButton()
-            }
+            memoTitleLabel.text = memoConfiguration.title
+            memoId = memoConfiguration.memoId
         }
     }
     
@@ -39,9 +31,9 @@ class MemoCellView: UIView, UIContentView  {
     
     init(configuration: MemoCellConfiguration) {
         super.init(frame: .zero)
-        self.configuration = configuration
         loadView()
         firstConfiguration()
+        self.configuration = configuration
     }
     
     required init?(coder: NSCoder) {
@@ -60,59 +52,35 @@ class MemoCellView: UIView, UIContentView  {
         ])
     }
     
-    func firstConfiguration() {
-   
-        configureLayout()
+    func createMenu() -> UIMenu {
+        var menuChildren = [UIMenuElement]()
+        
+        var deleteAction = UIAction(title: "削除",image: UIImage(systemName: "trash"), attributes: .destructive, handler: { [weak self] _ in
+            guard let self = self else { return }
+            deleteBlock?(memoId)
+        })
+        menuChildren.append(deleteAction)
+        
+        return UIMenu(title: "", options: .singleSelection, children: menuChildren)
     }
     
-    func configureLayout() {
+    func firstConfiguration() {
         
-        //memoTitleLabel
+        menuButton.menu = createMenu()
+   
         memoTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         memoTitleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 12).isActive = true
         memoTitleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
         memoTitleLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12).isActive = true
         memoTitleLabel.heightAnchor.constraint(equalToConstant: 30 * UIScreen.main.bounds.size.width / 390).isActive = true
         
-        //menuButton
         menuButton.translatesAutoresizingMaskIntoConstraints = false
         menuButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 12).isActive = true
         menuButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
         menuButton.leftAnchor.constraint(equalTo: memoTitleLabel.rightAnchor, constant: 1).isActive = true
         menuButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12).isActive = true
         menuButton.widthAnchor.constraint(equalToConstant: 30 * UIScreen.main.bounds.size.width / 390).isActive = true
-        
     }
-    
-    func configureMemoTitleLabel() {
-        memoTitleLabel.text = memoConfiguration.title
-    }
-    
-    func configureMemoButton() {
-        let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 25.0 * UIScreen.main.bounds.size.width / 390, weight: .regular, scale: .small)
-        let systemImage = UIImage(systemName: "ellipsis", withConfiguration: symbolConfiguration)
-        menuButton.setTitle("", for: .normal)
-        menuButton.setImage(systemImage, for: .normal)
-        menuButton.menu = createMenu()
-        menuButton.showsMenuAsPrimaryAction = true
-        menuButton.tintColor = UIColor.systemGray4
-    }
-    
-    func createMenu() -> UIMenu {
-        var menus = [UIMenuElement]()
-//        menus.append(UIAction(title: "移動", image: UIImage(systemName: "arrow.right"), handler: {_ in
-//            NotificationCenter.default.post(name: .notifyTransfer, object: nil)
-//        }))
-        menus.append(UIAction(title: "削除",image: UIImage(systemName: "trash"), attributes: .destructive, handler: { [weak self] _ in
-            guard let self = self else { return }
-            NotificationCenter.default.post(name: .notifyDeleteMemo, object: nil, userInfo: ["folderId": self.memoConfiguration.folderId!, "memoId": self.memoConfiguration.memoId!])
-            
-        }))
-        
-        return UIMenu(title: "", options: .singleSelection, children: menus)
-    }
-    
-    
 }
 
 extension Notification.Name {
