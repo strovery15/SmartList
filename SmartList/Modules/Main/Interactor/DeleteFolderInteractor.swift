248751aae382a8d1fffd3ff232584c1c188ee812
strovery15 <yutakawamae@gmail.com>
2025-11-16 00:00:18 +0900

    task-DeleteFolderInteractorでrealmFolderのdelete手順を変更した
    #与えられたidに沿ってfolderを消すのではなく、realmFolderIdsで消すidと同じfolderを消すように変更した

--- SmartList/Modules/Main/Interactor/DeleteFolderInteractor.swift
+++ SmartList/Modules/Main/Interactor/DeleteFolderInteractor.swift
@@ -17,10 +17,6 @@ class DeleteFolderInteractor: DeleteFolderUseCase {
         let realmIdManager = realm.objects(RealmIdManagerEntity.self).first!
         var firstIndex = 0
         
-        let realmFolderDelete = realmFolders.first(where: { $0.id == parameter })!
-        try! realm.write {
-            realm.delete(realmFolderDelete)
-        }
         for (index, realmFolderId) in realmIdManager.folderIds.enumerated() {
             if realmFolderId.id == parameter {
                 if realmIdManager.folderIds.last!.id == realmFolderId.id {
@@ -28,8 +24,9 @@ class DeleteFolderInteractor: DeleteFolderUseCase {
                 } else {
                     firstIndex = index
                 }
-                
+                let realmFolderDelete = realmFolders.first(where: { $0.id == realmFolderId.id })!
                 try! realm.write {
+                    realm.delete(realmFolderDelete)
                     realmIdManager.folderIds.remove(at: index)
                 }
             }
